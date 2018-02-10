a359b499045ab91d44dff3d8e2339cb82cd4b286
Unknown <lukecrum20@gmail.com>
2018-02-10 11:49:40 -0500

    idek

--- Big-Boy-5740/src/org/usfirst/frc/team5740/robot/subsystems/RobotObjects.java
+++ Big-Boy-5740/src/org/usfirst/frc/team5740/robot/subsystems/RobotObjects.java
@@ -14,12 +14,10 @@ public class RobotObjects {
 	public static Encoder e1 = new Encoder(0, 1);
 	public static Encoder e2 = new Encoder(2, 3);
 	public static DifferentialDrive drive = new DifferentialDrive(v1, v2);
-	public static Solenoid solenoid0 = new Solenoid(0);
-	public static Solenoid solenoid1 = new Solenoid(1);
-	public static Solenoid solenoid2 = new Solenoid(2);
-	public static Solenoid solenoid3 = new Solenoid(3);
-	public static Solenoid solenoid4 = new Solenoid(4);
-	public static DoubleSolenoid solenoid5 = new DoubleSolenoid(5, 6);
+	public static DoubleSolenoid solenoid0 = new DoubleSolenoid(0, 1);
+	public static DoubleSolenoid solenoid1 = new DoubleSolenoid(2, 3);
+	public static DoubleSolenoid solenoid2 = new DoubleSolenoid(4, 5);
+	public static DoubleSolenoid solenoid3 = new DoubleSolenoid(6, 7);
 	public static Joystick controller1 = new Joystick(0);
 	public static Joystick controller2 = new Joystick(1);
 	public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
