643e4449b0d7823a96c02d86354d8bc636643c5b
Unknown <lukecrum20@gmail.com>
2018-02-13 16:31:56 -0500

    constant bois

--- Big-Boy-5740/src/org/usfirst/frc/team5740/robot/Constants.java
+++ Big-Boy-5740/src/org/usfirst/frc/team5740/robot/Constants.java
@@ -3,5 +3,24 @@ package org.usfirst.frc.team5740.robot;
 public class Constants {
 	public static final Integer leftDriveMotorPWM = 0;
 	public static final Integer rightDriveMotorPWM = 1;
-			
+	public static final Integer encoderOnePortOne = 0;
+	public static final Integer encoderOnePortTwo = 1;
+	public static final Integer encoderTwoPortOne = 2;
+	public static final Integer encoderTwoPortTwo = 3;
+	public static final Integer clawSolenoidPortOne = 0;
+	public static final Integer clawSolenoidPortTwo = 1;
+	public static final Integer shiftSolenoidPortOne = 2;
+	public static final Integer shiftSolenoidPortTwo = 3;
+	public static final Integer elevatorSolenoidPortOne = 4;
+	public static final Integer elevatorSolenoidPortTwo = 5;
+	public static final Integer rampSolenoidPortOne = 6;
+	public static final Integer rampSolenoidPortTwo = 7;
+	public static final Integer controller1Port = 0;
+	public static final Integer controller2Port = 1;
+	public static final Integer eTalonLeftCAN = 1;
+	public static final Integer eTalonRightCAN = 2;
+	public static final Integer eVictorLeftCAN = 3;
+	public static final Integer eVictorRightCAN = 4;
+	public static final Integer clawIntakeSparkPWM = 2;
+	public static final Integer clawRotationSparkPWM = 3;
 }
