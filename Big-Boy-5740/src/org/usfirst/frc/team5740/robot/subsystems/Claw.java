36c36c6de53b1abe8704adc6f3ced603693efc1d
Jake Ball <spazmonkey214@gmail.com>
2018-02-20 19:31:54 -0500

    add claw encoder code

--- Big-Boy-5740/src/org/usfirst/frc/team5740/robot/subsystems/Claw.java
+++ Big-Boy-5740/src/org/usfirst/frc/team5740/robot/subsystems/Claw.java
@@ -4,11 +4,8 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team5740.robot.subsystems.RobotObjects;
 
-/**
- *
- */
 public class Claw extends Subsystem {
-
+	public static String last;
     public void initDefaultCommand() {
 
     }
@@ -22,6 +19,27 @@ public class Claw extends Subsystem {
     
     public static void retract() {
     	RobotObjects.clawSolenoid.set(DoubleSolenoid.Value.kReverse);    		
+    }
+    public static void moveClaw(String direction) {
+    	RobotObjects.clawRotationEncoder.reset();
+    	switch(direction) {
+    	case "up":
+    		if(last == "down") {
+    		while(RobotObjects.clawRotationEncoder.get() < 14200) {
+    			RobotObjects.clawRotation.setSpeed(1);
+    		}
+    		last = "up";
+    		}
+
+    		
+    	case "down":
+    		if(last == "up") {
+    		while(RobotObjects.clawRotationEncoder.get() > -14200) {
+    			RobotObjects.clawRotation.setSpeed(-1);
+    		}
+    		last = "down";
+    	}
+    	}
     }
 }
 
